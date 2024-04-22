a480e39d649ef8221f84662e98e326060a2e855e
bravothegreat <okwudilijohngreat@gmail.com>
2024-04-22 11:07:20 +0100

    all-my-github-respo

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,5 @@
 
 
-// import {  Routes, Route, NavLink, BrowserRouter, Router, Link } from "react-router-dom"
-// 
-// import Navbar from "./component/Navbar"
-// // import About from "./component/About"
-// // import Contact from "./component/Contact"
-
-
-// function App() {   
-//   return (
-//     <>
-//       <Navbar />
-     
-
-//     </>
-    
-//   )
-// }
-
-// export default App
-
 
 import Navbar from './component/Navbar';
 import { ChakraProvider } from '@chakra-ui/react';
